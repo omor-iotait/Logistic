79f203a8f07d010b176ac66972a4fed481a28a0c
omor-iotait <56671984+omor-iotait@users.noreply.github.com>
2019-12-23 13:32:11 +0600

    Structure Update

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,5 @@
 <?php
-$con=new mysqli("localhost","root","", "db_warehouse");
+$con=new mysqli("localhost","root","", "db_logistic");
 //$con=new mysqli("localhost","musltwlf_omor","123@123z", "musltwlf_logistic");
 if($con === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
